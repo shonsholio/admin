a99b1fb3c474aca031b1155539af865ce623178d
Luis Fernando Arevalo Uribe <LuisferArevalo@Luiss-iMac.local>
2026-05-01 09:28:36 -0500

    con sharon

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ app.use(cors())
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './views'))
 
+const data = require('./src/data.json')
+
 app.get('/', (req,res) => {
-  res.render('postulantes')
+  res.render(
+    'postulantes', {
+      data: data
+    }
+    )
 })
 
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
